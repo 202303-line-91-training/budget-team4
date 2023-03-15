f6a2a6fe934fa512c359e7b5639cdd732645b7e8
Joey Chen <jay.hatelove@gmail.com>
2023-03-15 22:00:10 +0800

    use Period domain logic for overlappingStart

--- src/Period.js
+++ src/Period.js
@@ -11,15 +11,17 @@ class Period {
         let overlappingEnd = this.endDate.isBefore(budget.lastDay())
             ? this.endDate
             : budget.lastDay();
-        let overlappingStart;
+        let overlappingStart = this.startDate.isAfter(budget.firstDay())
+            ? this.startDate
+            : budget.firstDay();
         if (budget.yearMonth === this.startDate.format('YYYYMM')) {
             // overlappingEnd = budget.lastDay();
-            overlappingStart = this.startDate;
+            // overlappingStart = this.startDate;
         } else if (budget.yearMonth === this.endDate.format('YYYYMM')) {
-            overlappingStart = budget.firstDay();
+            // overlappingStart = budget.firstDay();
             // overlappingEnd = this.endDate;
         } else {
-            overlappingStart = budget.firstDay();
+            // overlappingStart = budget.firstDay();
             // overlappingEnd = budget.lastDay();
         }
         return overlappingEnd.diff(overlappingStart, 'day') + 1;
